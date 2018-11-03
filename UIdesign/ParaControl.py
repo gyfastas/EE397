c407d0d83f29bd456dc296c7f0babf0b129496b7
gyfastas <35861032+gyfastas@users.noreply.github.com>
2018-11-03 21:49:02 +0800

    update UI

---
+++ UIdesign/ParaControl.py
@@ -0,0 +1,13 @@
+'''
+EE397
+Pyqt5 Car control module
+'''
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtNetwork import *
+import socket
+import sys
+
