3c2c96b5d96c59a9ed9b46b2eceaf34d44a14b16
gyfastas <35861032+gyfastas@users.noreply.github.com>
2018-11-12 13:27:08 +0800

    update
    1. menu

---
+++ TopDesign.cpp
@@ -0,0 +1,229 @@
+#include <Wire.h>
+#include <WiFi.h>
+#include <EEPROM.h>
+#include <MPU6050.h>
+#include <Kalman.h>
+#include <TB6612FNG.h>
+#include <Bala.h>
+#include <Flash.h>
+
+#define EEPROM_SIZE 12
+#define EEPROM_FLAG 0x01
+
+#define STANDBY 15
+#define MOTORL_IN1 5
+#define MOTORL_IN2 18
+#define PWML 19
+#define MOTORR_IN1 4
+#define MOTORR_IN2 16
+#define PWMR 17
+
+#define NUMSSID 2
+const String ssid[NUMSSID] = { "WiFiSSID1", "WiFiSSID2" };
+const String pswd[NUMSSID] = { "WiFIPSWD1", "WiFIPSWD2" };
+
+const char* host     = "REMOTETCP";
+const int   hostPort = 80;
+
+const char* TCPCert  = "USERNAME AND PASSWORD";
+
+WiFiClient client;
+MPU6050 mpu;
+Kalman kfr, kfp;
+Tb6612fng motorsDriver(STANDBY, MOTORL_IN1, MOTORL_IN2, PWML, MOTORR_IN1, MOTORR_IN2, PWMR);
+Bala myBala(mpu, kfr, kfp, motorsDriver, Wire);
+Flash myFlash(EEPROM_SIZE, EEPROM_FLAG);
+
+void showParaK(uint8_t K)
+{	
+	String info = "";
+	switch(K)
+	{
+        case 0 : info += "\r\nKp_B: ";        info += myBala.getParaK(0); 	break;
+        case 1 : info += "\r\nKd_B: ";        info += myBala.getParaK(1); 	break;
+        case 2 : info += "\r\nKp_V: ";        info += myBala.getParaK(2); 	break;
+        case 3 : info += "\r\nKi_V: ";        info += myBala.getParaK(3); 	break;
+        case 4 : info += "\r\nKd_V: ";        info += myBala.getParaK(4); 	break;
+        case 5 : info += "\r\nKp_T: ";        info += myBala.getParaK(5); 	break;
+        case 6 : info += "\r\nKi_T: ";        info += myBala.getParaK(6); 	break;
+        case 7 : info += "\r\nKd_T: ";        info += myBala.getParaK(7); 	break;
+        case 8 : info += "\r\nSDK: ";         info += myBala.getParaK(8); 	break;
+        case 9 : info += "\r\nTargetAngle: "; info += myBala.getParaK(9); 	break;
+        case 10: info += "\r\nVecPeriod: ";   info += myBala.getParaK(10); 	break;
+        case 11: info += "\r\nCarDown: ";     info += myBala.getParaK(11); 	break;
+		default:															break; 
+	}
+	client.println(info.c_str());
+}
+void WiFiConfig()
+{
+	static uint8_t connection_timeout_cnt = 0;
+	for (uint8_t idx = 0; idx < NUMSSID; ++idx)
+	{
+		Serial.println();
+		Serial.print("Connecting to ");
+		Serial.println(ssid[idx]);
+
+		WiFi.begin(ssid[idx].c_str(), pswd[idx].c_str());
+
+		connection_timeout_cnt = 0;
+
+		while (WiFi.status() != WL_CONNECTED) 
+		{
+			delay(500);
+			if (++connection_timeout_cnt >= 4)
+				break;
+			Serial.print(".");
+		}
+
+		if (WiFi.status() == WL_CONNECTED)
+			break;
+	}
+
+	if (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.println();
+		Serial.println("WiFi connection failed.");
+		return;
+	}
+
+	Serial.println();
+	Serial.println("WiFi connected");
+
+	Serial.print("Connecting to remote TCP server ");
+	Serial.println(host);
+
+	if (!client.connect(host, hostPort)) 
+	{
+		Serial.println("TCP Server connection failed");
+		return;
+	}
+
+	// Send username and password to login
+	client.print(TCPCert);
+}
+
+void remoteControl(void *parameter)
+{
+  // Connect to WiFi route and the remote TCP server
+  WiFiConfig();
+  while(1)
+  {
+    // Get command from remote controller via WiFi
+    if (client.available())
+    {
+      String cmd = client.readStringUntil('\r');
+      cmd.trim();
+      Serial.print("\nGet command: ");
+      Serial.println(cmd);
+      if (cmd.substring(0,1) == String("#"))
+      {	
+		if cmd.length()<2 
+		{
+        String info = "Angle: ";     info += myBala.getRoll();
+        info += "\r\nKp_B: ";        info += myBala.getParaK(0);
+        info += "\r\nKd_B: ";        info += myBala.getParaK(1);
+        info += "\r\nKp_V: ";        info += myBala.getParaK(2);
+        info += "\r\nKi_V: ";        info += myBala.getParaK(3);
+        info += "\r\nKd_V: ";        info += myBala.getParaK(4);
+        info += "\r\nKp_T: ";        info += myBala.getParaK(5);
+        info += "\r\nKi_T: ";        info += myBala.getParaK(6);
+        info += "\r\nKd_T: ";        info += myBala.getParaK(7);
+        info += "\r\nSDK: ";         info += myBala.getParaK(8);
+        info += "\r\nTargetAngle: "; info += myBala.getParaK(9);
+        info += "\r\nVecPeriod: ";   info += myBala.getParaK(10);
+        info += "\r\nCarDown: ";     info += myBala.getParaK(11);
+        info += "\r\nSpeedL: ";      info += myBala.getSpeedL();
+        info += "\r\nSpeedR: ";      info += myBala.getSpeedR();
+        client.println(info.c_str());
+        for (uint8_t i = 0; i < EEPROM_SIZE; ++i)
+          Serial.println(myBala.getParaK(i));
+		}
+		else
+		{
+		K = cmd.substring(1).toInt()-1;
+		showParaK(K);
+		}
+      }
+	  
+      else
+      {
+      	uint8_t ty = cmd.indexOf('#') - 1;
+		
+      	if (ty == 0)
+        	myBala.setParaK((uint8_t)(cmd.substring(0,1).toInt() - 1), cmd.substring(1).toFloat());
+        else if (ty == 1)
+        	myBala.setParaK((uint8_t)(cmd.substring(0,2).toInt() - 1), cmd.substring(2).toFloat());
+        myFlash.updateEEPROM(myBala);    
+      }
+    } 
+    vTaskDelay(10);
+  }
+  vTaskDelete(NULL);  
+}
+
+void balaControl(void *parameter)
+{
+  // Initialize bala ...
+  myBala.begin();
+  delay(500);
+
+  // Update PID parameters ...
+  if (myFlash.initEEPROM(myBala))
+  {
+    Serial.println("Restart ...");
+    ESP.restart();
+  }
+
+  while(1)
+  {
+    static uint32_t control_interval = millis() + 5;
+    if (millis() > control_interval) 
+    {
+      control_interval = millis() + 5;
+      myBala.run();
+    }  
+    vTaskDelay(10); 
+  }
+  vTaskDelete(NULL);  
+}
+
+void setup() 
+{
+  // Power on stabilizing ...
+  delay(500);
+
+  // Start I2C bus
+  Wire.begin(/*SDA*/21, /*SCL*/22);
+  Wire.setClock(400000UL);          // Set I2C frequency to 400kHz
+  delay(500);
+
+  // Start Serial for diplay debug message
+  Serial.begin(115200);
+  delay(500);
+
+  // Create a task on RTOS for self-balance car control
+  xTaskCreatePinnedToCore(
+    balaControl,          /* Task function. */
+    "balaControl",        /* String with name of task. */
+    10000,                /* Stack size in words. */
+    NULL,                 /* Parameter passed as input of the task */
+    2,                    /* Priority of the task. */
+    NULL,                 /* Task handle. */
+    1);                   /* Run on core 1. */
+
+  // Create a task on RTOS for WiFi event
+  xTaskCreatePinnedToCore(
+    remoteControl,        /* Task function. */
+    "remoteControl",      /* String with name of task. */
+    10000,                /* Stack size in words. */
+    NULL,                 /* Parameter passed as input of the task */
+    1,                    /* Priority of the task. */
+    NULL,                 /* Task handle. */
+    0);                   /* Run on core 0. */
+}
+
+void loop() 
+{
+
+}
--- UIdesign/MainWindow_2.py
+++ UIdesign/MainWindow_2.py
@@ -19,6 +19,9 @@ class MainWindow(QWidget):
         self.isConnect = False
         self.setupUi()
         self.setupSlot()
+        self.initMenu()
+        self.initAnimation()
+
     def setupUi(self):
         self.resize(1069, 788)
         self.upButton = QtWidgets.QPushButton(self)
@@ -123,6 +126,31 @@ class MainWindow(QWidget):
 
         #setup slider
         self.setSlider()
+
+    def contextMenuEvent(self, event):
+        pos = event.globalPos()
+        size = self._contextMenu.sizeHint()
+        x, y, w, h = pos.x(), pos.y(), size.width(), size.height()
+        self._animation.stop()
+        self._animation.setStartValue(QRect(x, y, 0, 0))
+        self._animation.setEndValue(QRect(x, y, w, h))
+        self._animation.start()
+        self._contextMenu.exec_(event.globalPos())
+
+    def initMenu(self):
+        self._contextMenu = QMenu(self)
+        self.ac_getAllPara = self._contextMenu.addAction('回传所有参数', self.GetAllPara)
+
+    def GetAllPara(self):
+        self.checkcmd('###')
+
+    def initAnimation(self):
+        # 按钮动画
+        self._animation = QPropertyAnimation(
+            self._contextMenu, b'geometry', self,
+            easingCurve=QEasingCurve.Linear, duration=300)
+        # easingCurve 修改该变量可以实现不同的效果
+
 
     def setupSlot(self):
         #Slot connect#
