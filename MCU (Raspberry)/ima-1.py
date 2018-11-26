910f18d37c7c13569ea91f6e929f76dcff01b4e1
gyfastas <35861032+gyfastas@users.noreply.github.com>
2018-11-26 13:56:19 +0800

    Update

--- MCU (Raspberry)/ima-1.py
+++ MCU (Raspberry)/ima-1.py
@@ -18,7 +18,7 @@ try:
         img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # 普通二值化处理 thresh1即为得到图像
-        thre1 = 127
+        thre1 = 80
         ret, thresh1 = cv2.threshold(img1, thre1, 255, cv2.THRESH_BINARY)
 
         # 高斯模糊canny里面设置上下限阈值，canny为输出图像
@@ -26,9 +26,12 @@ try:
         canny = cv2.Canny(blur, 50, 200)
 
         # if
+        cv2.namedWindow("Face Detector")
+        cv2.namedWindow("二值化图像")
+        cv2.namedWindow("高斯模糊图像")
         cv2.imshow("Face Detector", img1)
         cv2.imshow("二值化图像", thresh1)
-        cv2.imshow("高斯模糊图像", thresh1)
+        cv2.imshow("高斯模糊图像", canny)
 
         cv2.waitKey(1)
 finally:
