a334d071a2b9869278d9d9f6419d0066cabbe4c7
rainfall1998 <43332711+rainfall1998@users.noreply.github.com>
2018-12-10 22:41:40 +0800

    Update tracking_1.py
    已经基本完成寻线功能，需要解决线程崩溃问题

--- MCU (Raspberry)/tracking_1.py
+++ MCU (Raspberry)/tracking_1.py
@@ -10,10 +10,33 @@ import numpy as np, cv2, sys
 # output = picamera.array.PiRGBArray(camera)
 # camera.resolution = (320, 240) # 图像大小
 # camera.framerate=10 # 帧
+# 定义sobel算子
+sobel_r = np.array([[0,0,0],[-1,0,1],[0,0,0]])
+sobel_l = np.array([[0,0,0],[1,0,-1],[0,0,0]])
+
+'''
+垂直边缘检测
+'''
+def edgeDetection(img):
+    '''
+
+    :param img: 二值化之后的图像
+    :return: 边缘检测图像
+    '''
+    edge_L = cv2.filter2D(img,sobel_l)
+    edge_R = cv2.filter2D(img,sobel_r)
+    edge = edge_L + edge_R
+
+    return edge
+
+
+
 queueLock = mp.Lock()
+queueLockgray = mp.Lock()
 
 # 打开串口
 ser = serial.Serial("/dev/ttyAMA0", 115200)
+
 
 #x是16进制数：F:0x46；B:0x42；R:0x52；L:0x49；
 def send(x):
@@ -47,7 +70,7 @@ def blackline(imag, th, wi, hi):
     #黑线中心计算
     i2 = 0
     for i1 in range(1, 10):
-        i = int((i1)*hi / 30) - 1
+        i = int((i1+70)*hi / 90) - 1
         j = 0
 
         while ((imag[i, j] > th) or (imag[i, j+3] > th)) and (j < wi-3):
@@ -57,6 +80,7 @@ def blackline(imag, th, wi, hi):
             j1 = j1+1
         if j < wi-3:
             mid[i2] = [i,int((j+j1)/2)]
+            imag[i,int((j+j1)/2)] = 255
             i2 = i2 +1
             
     #偏差角计算
@@ -66,62 +90,68 @@ def blackline(imag, th, wi, hi):
         tan = 0
         m = int(i2/2)
         for ii in range(0, m):
-            tan += (mid[ii+m, 1] - mid[ii, 1])*30 / hi
-        tan = tan / m
+            tan += (mid[ii+m, 1] + mid[ii, 1]) - 2*int(wi/2)
+        tan = tan / m /2
     print('tan=')
     print(tan)
-    return tan
+    return imag,tan
 
 #图像读取分析
-def imageanalyze(imgQueue,img):
-    th = 30  # 灰度阈值
-    th1 = 3  # 角度阈值
+def imageanalyze(imgQueue,img,imgQueuegray):
+    th = 80  # 灰度阈值
+    th1 = 20  # 角度阈值
     img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#灰度化
     #图像缩小
     sp = img1.shape
     wi = int(sp[1] * 0.8)
     hi = int(sp[0] * 0.8)
     img2 = cv2.resize(img1, (wi, hi))
-    queueLock.acquire()
-    imgQueue.put(img2)
-    queueLock.release()
-    #图像分析
-    tan = blackline(img2, th, wi, hi)
+    thre1 = 80
+    ret, thresh1 = cv2.threshold(img2, thre1, 255, cv2.THRESH_BINARY)
+    queueLockgray.acquire()
+    imgQueuegray.put(thresh1)
+    queueLockgray.release()
     
+    #图像分析
+    [img3,tan] = blackline(img2, th, wi, hi)
+    queueLock.acquire()
+    imgQueue.put(img3)
+    queueLock.release()
     mes = 0x53
     if tan > th1:
-       mes = 0x53#2 #R
+       mes = 0x4c#2 #R
     if tan < 0-th1:
-       mes = 0x53#0x4c #L
+       mes = 0x52#0x4c #L
 
     if tan <= th1 and tan >= -th1:
-       mes = 0x53#0x46#F
+       mes = 0x46#0x46#F
     return mes  # , second
 
-def motionControl(enable, imgQueue):
-    try:
-        camera = PiCamera()
-        output = picamera.array.PiRGBArray(camera)
-        camera.resolution = (320, 240) # 图像大小
-        camera.framerate=10 # 帧
-        while enable.value == 1:
-            camera.capture(output, 'bgr')
+def motionControl(enable, imgQueue,imgQueuegray):
+    
+    camera = PiCamera()
+    output = picamera.array.PiRGBArray(camera)
+    camera.resolution = (320, 240) # 图像大小
+    camera.framerate=40 # 帧
+    while enable.value == 1:
+        for frame in camera.capture_continuous(output, format="bgr", use_video_port=True):
             img = output.array
-            mes = imageanalyze(imgQueue,img)
+            mes = imageanalyze(imgQueue,img,imgQueuegray)
             output.truncate(0)
             send(0x53)
             send(mes)
             #time.sleep(0.5)
-    except:
-        enable.value = 0
-        print('control')
-        send(0x45) # some wrong with device, send error message and quit the thread
+    # except:
+    #     enable.value = 0
+    #     print('control')
+    #     send(0x45) # some wrong with device, send error message and quit the thread
     camera.close()
     print('Quit motionControl')
 
-def displayImage(enable, imgQueue):
+def displayImage(enable, imgQueue,imgQueuegray):
     while enable.value == 1:
         img = None
+        img2 = None
         queueLock.acquire()
         if not imgQueue.empty():
             img = imgQueue.get()
@@ -131,12 +161,23 @@ def displayImage(enable, imgQueue):
         if img is not None:
             cv2.imshow('Tracking', img)
             cv2.waitKey(1)
+        #二值化图
+        queueLockgray.acquire()
+        if not imgQueuegray.empty():
+            img2 = imgQueuegray.get()
+            queueLockgray.release()
+        else:
+            queueLockgray.release()
+        if img2 is not None:
+            cv2.imshow('Trackinggray', img2)
+            cv2.waitKey(1)
     print('Quit displayImage')
     cv2.destroyAllWindows()
 
 def main():
     enable = mp.Value('i', 0)
     imgQueue = mp.Queue(0)
+    imgQueuegray = mp.Queue(0)
     while True:
         #判断是否开始
         sign = receive()
@@ -144,8 +185,8 @@ def main():
             print('connect')
             # try:
             enable.value = 1
-            mp.Process(target=motionControl, args=(enable, imgQueue)).start()
-            mp.Process(target=displayImage, args=(enable, imgQueue)).start()
+            mp.Process(target=motionControl, args=(enable, imgQueue,imgQueuegray)).start()
+            mp.Process(target=displayImage, args=(enable, imgQueue,imgQueuegray)).start()
             # except:
             #     enable.value = 0
             #     print('main')
